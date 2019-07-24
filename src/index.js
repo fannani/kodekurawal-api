563bda770fbde47f3757947bc8ca47932313992b
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-07-24 14:47:08 +0700

    fix graphql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const { ApolloServer } = require('apollo-server');
 const {importSchema} = require('graphql-import');
+const { GraphQLUpload } = require('graphql-upload');
+
 
 const typeDefs = importSchema(__dirname + '/schema.graphql')
-const server = new ApolloServer({ typeDefs });
+console.log(typeDefs);
+const resolvers = {
+    Upload: GraphQLUpload
+}
+const server = new ApolloServer({ typeDefs,resolvers });
 server.listen().then(({url}) => {
     console.log(`🚀 Server ready at ${url}`)
 })
