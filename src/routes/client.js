883707b60c014f147c517e473e98faf874eca776
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-09-11 01:08:58 +0700

    fixing client

--- src/routes/client.js
+++ src/routes/client.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 		<div align="center">
 			___________________
 			<br/>
-			Welcome to KodeKurawal API 1.2.1
+			Welcome to KodeKurawal API 1.2.2
 			<br/>
 			______🚀😀🚀_____
 		</div>
