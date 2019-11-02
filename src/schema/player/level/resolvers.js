88d574ef65f01d789ae3e6b35b1e6b888e517011
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-11-03 00:29:24 +0700

    fix game settings

--- src/schema/player/level/resolvers.js
+++ src/schema/player/level/resolvers.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 const PlayerLevel = mongoose.model("PlayerLevel")
 const resolvers = {
   Query : {
-    playerLevel : () =>{
+    playerLevel : (_,args) =>{
       return PlayerLevel.find(args);
     }
   },
   Mutation : {
-    addPlayerLevel : ({ level, exp_req }) => {
-      const newdata = new UserLevel({
+    addPlayerLevel : (_,{ level, exp_req }) => {
+      const newdata = new PlayerLevel({
         level,
         exp_req,
       });
